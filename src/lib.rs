cd8d525be65e9dcadc8d03bfe34e37345d7a64bb
Emily Crandall Fleischman <emilycf@mit.edu>
2022-03-11 12:07:00 -0500

    Support multiple strs in a single macro expression

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,38 @@
-use quote::quote_spanned;
-use syn::{parse_macro_input, Error, Lit};
+use quote::quote;
+use syn::{
+    parse_macro_input, parse_quote_spanned,
+    visit_mut::{self, VisitMut},
+    Expr, Lit,
+};
 
-/// Converts a `&str` literal to `&[char]`.
+/// Converts `&str` literals to `&[char]`.
 ///
 /// ```rust
 /// use utf32_lit::utf32;
 /// let s = utf32!("æbc");
 /// assert_eq!(s, &['æ', 'b', 'c']);
+///
+/// let s_array = utf32!(&["foo", "bar", "baz"]);
+/// assert_eq!(s_array, &[&['f', 'o', 'o'], &['b', 'a', 'r'], &['b', 'a', 'z']]);
 /// ```
 #[proc_macro]
 pub fn utf32(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    match parse_macro_input!(input as Lit) {
-        Lit::Str(s) => {
-            let string = s.value();
-            let chars = string.chars();
-            quote_spanned!(s.span()=> &[#(#chars),*])
+    let mut expr = parse_macro_input!(input as Expr);
+    Utf32Replace.visit_expr_mut(&mut expr);
+    quote!(#expr).into()
+}
+
+struct Utf32Replace;
+
+impl VisitMut for Utf32Replace {
+    fn visit_expr_mut(&mut self, node: &mut Expr) {
+        if let Expr::Lit(expr) = &node {
+            if let Lit::Str(s) = &expr.lit {
+                let string = s.value();
+                let chars = string.chars();
+                *node = parse_quote_spanned!(s.span()=> &[#(#chars),*]);
+            }
         }
-        other => {
-            let message = "expected string literal";
-            Error::new_spanned(other, message).to_compile_error()
-        }
+        visit_mut::visit_expr_mut(self, node);
     }
-    .into()
 }
